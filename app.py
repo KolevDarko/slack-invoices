3fe9eb663b0e3413beca6a1d1f582f8cc77f44ee
Darko Kolev <kolevdarko@gmail.com>
2024-11-08 09:17:39 +0100

    updated workflow and checking slack request signature

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+from functools import wraps
 import os
+import time
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
+from slack_sdk.signature import SignatureVerifier
 from slack_bolt.adapter.flask import SlackRequestHandler
 from slack_bolt import App
 from dotenv import find_dotenv, load_dotenv
-from flask import Flask, jsonify, request
+from flask import Flask, abort, jsonify, request
 from functions import extract_invoice_data
 from invoice import create_invoice
 
@@ -27,11 +30,42 @@ handler = SlackRequestHandler(app)
 def get_bot_user_id():
     # Gets bot user id via slack Api
     try:
-        slack_client = WebClient(token=os.environ["SLACK_BOT_TOKEN"])
+        slack_client = WebClient(token=SLACK_BOT_TOKEN)
         response = slack_client.auth_test()
         return response["user_id"]
     except SlackApiError as e:
         print(f"Error: {e}")
+
+
+signature_verifier = SignatureVerifier(SLACK_SIGNING_SECRET)
+
+
+def require_slack_verification(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not verify_slack_request():
+            abort(403)
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+def verify_slack_request():
+    # Get the request headers
+    timestamp = request.headers.get("X-Slack-Request-Timestamp", "")
+    signature = request.headers.get("X-Slack-Signature", "")
+
+    # Check if the timestamp is within five minutes of the current time
+    current_timestamp = int(time.time())
+    if abs(current_timestamp - int(timestamp)) > 60 * 5:
+        return False
+
+    # Verify the request signature
+    return signature_verifier.is_valid(
+        body=request.get_data().decode("utf-8"),
+        timestamp=timestamp,
+        signature=signature,
+    )
 
 
 @app.event("app_mention")
@@ -58,6 +92,7 @@ def handle_mentions(body, say):
 
 
 @flask_app.route("/slack/events", methods=["POST"])
+@require_slack_verification
 def slack_events():
     """
     Route for handling Slack events.
@@ -84,4 +119,4 @@ def hello():
 
 # Run the Flask app
 if __name__ == "__main__":
-    flask_app.run(port=5005, debug=True)
+    flask_app.run(host="0.0.0.0", port=8000)
